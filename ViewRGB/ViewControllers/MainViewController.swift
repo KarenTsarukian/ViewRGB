93c6cd73c1cb65f1ebadf203528787a6e3c1fc64
Karen Tsarukyan <tsarukyan2016@icloud.com>
2022-03-22 22:21:44 +0500

    Problem

---
+++ ViewRGB/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  ViewRGB
+//
+//  Created by Karen Tsarukian on 13.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
